3be02742a1426c9158877f84c71cc8aba6f80f5b
Eryk <saintowskyy@gmail.com>
2025-07-16 21:28:23 +0200

    Updated mainyml

--- Backend/Models/Reservation.cs
+++ Backend/Models/Reservation.cs
@@ -13,10 +13,10 @@ namespace Backend.Models
         public string RoomName { get; set; } = string.Empty;
 
         [Required]
-        public string CheckIn { get; set; }
+        public required string CheckIn { get; set; }
 
         [Required]
-        public string CheckOut { get; set; }
+        public required string CheckOut { get; set; }
 
         [Range(1, 10)]
         public int Guests { get; set; }
